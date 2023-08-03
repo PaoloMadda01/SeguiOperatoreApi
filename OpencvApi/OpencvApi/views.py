2450a3fc89a16736e216a72b14361d553585c36e
Paolo Maddalosso <109733062+PaoloMadda01@users.noreply.github.com>
2023-08-03 21:01:19 +0200

    Training using mrdbourke's code

--- OpencvApi/OpencvApi/views.py
+++ OpencvApi/OpencvApi/views.py
@@ -21,7 +21,10 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 import serial
 import serial.tools.list_ports
+from timeit import default_timer as timer
 import time
+# Import accuracy metric
+from helper_functions import accuracy_fn
 
 # python3 manage.py runserver 192.168.181.129:8000
 
@@ -348,16 +351,82 @@ def update_model(request):
 
 
 
-def create_new_model():
-    num_classes = 2
+# def create_new_model():
+#    num_classes = 2
     # Carica una CNN pre-addestrata
-    model = models.resnet18(pretrained=True)
+#    model = models.resnet18(pretrained=True)
 
     # Sostituisci l'ultimo strato completamente connesso per adattarlo al numero di classi del tuo problema
-    num_ftrs = model.fc.in_features
-    model.fc = torch.nn.Linear(num_ftrs, num_classes)
+#    num_ftrs = model.fc.in_features
+#    model.fc = torch.nn.Linear(num_ftrs, num_classes)
 
+#    return model
+
+
+
+
+# *************   NEW     NEW     NEW     *****************
+def create_new_model():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    # Create a convolutional neural network 
+    class FashionMNISTModelV2(nn.Module):
+        """
+        Model architecture copying TinyVGG from: 
+        https://poloclub.github.io/cnn-explainer/
+        """
+        def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+            super().__init__()
+            self.block_1 = nn.Sequential(
+                nn.Conv2d(in_channels=input_shape, 
+                            out_channels=hidden_units, 
+                            kernel_size=3, # how big is the square that's going over the image?
+                            stride=1, # default
+                            padding=1),# options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+                nn.ReLU(),
+                nn.Conv2d(in_channels=hidden_units, 
+                          out_channels=hidden_units,
+                          kernel_size=3,
+                          stride=1,
+                          padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2,
+                             stride=2) # default stride value is same as kernel_size
+            )
+            self.block_2 = nn.Sequential(
+                nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2)
+            )
+            self.classifier = nn.Sequential(
+                nn.Flatten(),
+                # Where did this in_features shape come from? 
+                # It's because each layer of our network compresses and changes the shape of our inputs data.
+                nn.Linear(in_features=hidden_units*7*7, 
+                          out_features=output_shape)
+            )
+    
+        def forward(self, x: torch.Tensor):
+            x = self.block_1(x)
+            # print(x.shape)
+            x = self.block_2(x)
+            # print(x.shape)
+            x = self.classifier(x)
+            # print(x.shape)
+            return x
+
+    torch.manual_seed(42)
+    model = FashionMNISTModelV2(input_shape=1, 
+        hidden_units=10, 
+        output_shape=len(class_names)).to(device)
+    
     return model
+
+
+
+
 
 def retrain_method(model, photos_now, indexPhoto):
 
@@ -438,30 +507,72 @@ def retrain_model(model, photo_now, photo_incorrect):
     combined_dataset = torch.utils.data.ConcatDataset([dataset_now, dataset_incorrect])
 
     # Training the model
-    loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    train_loader = torch.utils.data.DataLoader(combined_dataset, batch_size=1, shuffle=True)
+    # loss_fn = torch.nn.CrossEntropyLoss()
+    # optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    # train_loader = torch.utils.data.DataLoader(combined_dataset, batch_size=1, shuffle=True)
 
-    inputs, labels = data
-    inputs, labels = inputs.to(device), labels.to(device)
+    # inputs, labels = data
+    # inputs, labels = inputs.to(device), labels.to(device)
 
-    for epoch in range(1):
-        running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
-            model.train()
+    # for epoch in range(1):
+        # running_loss = 0.0
+        # for i, data in enumerate(train_loader, 0):
+            # model.train()
 
-            y_logits = model(inputs)
-            y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+            # y_logits = model(inputs)
+            # y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
 
-            loss =loss_fn(y_logits, inputs)
+            # loss =loss_fn(y_logits, inputs)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            # optimizer.zero_grad()
+            # loss.backward()
+            # optimizer.step()
 
 
-            running_loss += loss.item()
-        print(f"Epoch {epoch + 1} loss: {running_loss / len(train_loader)}")
+            # running_loss += loss.item()
+        # print(f"Epoch {epoch + 1} loss: {running_loss / len(train_loader)}")
+
+
+
+    ###     NEW     NEW     NEW
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # Create data loaders
+    batch_size = 1  # You may adjust this based on your requirements
+    data_loader = DataLoader(combined_dataset, batch_size=batch_size, shuffle=True)
+
+    # Setup loss and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(params=model_2.parameters(), 
+                             lr=0.1)
+
+    # Set the seed and start the timer
+    torch.manual_seed(42)
+    train_time_start_on_cpu = timer()
+
+    # Set the number of epochs (we'll keep this small for faster training times)
+    epochs = 3
+
+    for epoch in tqdm(range(epochs)):
+        print(f"Epoch: {epoch}\n---------")
+        train_step(data_loader=data_loader, 
+                    model=model, 
+                    loss_fn=loss_fn,
+                    optimizer=optimizer,
+                    accuracy_fn=accuracy_fn,
+                    device=device
+                    )
+        # test_step(data_loader=test_dataloader,
+                    # model=model,
+                    # loss_fn=loss_fn,
+                    # accuracy_fn=accuracy_fn,
+                    # device=device
+                    # )
+
+    train_time_end_model = timer()
+    total_train_time_model = print_train_time(start=train_time_start_model,
+                                           end=train_time_end_model,
+                                           device=device)
 
     return model
 
@@ -782,3 +893,110 @@ def Print_Serial(x_coordinate, y_coordinate, distance, positive_prob):
     ser.write(data.encode())
 
 
+
+
+
+
+
+
+
+
+
+########    NEW     NEW         NEW         NEW     #######
+
+
+def print_train_time(start: float, end: float, device: torch.device = None):
+    """Prints difference between start and end time.
+
+    Args:
+        start (float): Start time of computation (preferred in timeit format). 
+        end (float): End time of computation.
+        device ([type], optional): Device that compute is running on. Defaults to None.
+
+    Returns:
+        float: time between start and end in seconds (higher is longer).
+    """
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds")
+    return total_time
+
+
+
+
+
+def train_step(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn,
+               device: torch.device = device):
+    train_loss, train_acc = 0, 0
+    model.to(device)
+    for batch, (X, y) in enumerate(data_loader):
+        # Send data to GPU
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass
+        y_pred = model(X)
+
+        # 2. Calculate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+        train_acc += accuracy_fn(y_true=y,
+                                 y_pred=y_pred.argmax(dim=1)) # Go from logits -> pred labels
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+    # Calculate loss and accuracy per epoch and print out what's happening
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
+
+def test_step(data_loader: torch.utils.data.DataLoader,
+              model: torch.nn.Module,
+              loss_fn: torch.nn.Module,
+              accuracy_fn,
+              device: torch.device = device):
+    test_loss, test_acc = 0, 0
+    model.to(device)
+    model.eval() # put model in eval mode
+    # Turn on inference context manager
+    with torch.inference_mode(): 
+        for X, y in data_loader:
+            # Send data to GPU
+            X, y = X.to(device), y.to(device)
+            
+            # 1. Forward pass
+            test_pred = model(X)
+            
+            # 2. Calculate loss and accuracy
+            test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(y_true=y,
+                y_pred=test_pred.argmax(dim=1) # Go from logits -> pred labels
+            )
+        
+        # Adjust metrics and print out
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)
+        print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
+
+
+import requests
+from pathlib import Path 
+
+# Download helper functions from Learn PyTorch repo (if not already downloaded)
+if Path("helper_functions.py").is_file():
+  print("helper_functions.py already exists, skipping download")
+else:
+  print("Downloading helper_functions.py")
+  # Note: you need the "raw" GitHub URL for this to work
+  request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+  with open("helper_functions.py", "wb") as f:
+    f.write(request.content)
